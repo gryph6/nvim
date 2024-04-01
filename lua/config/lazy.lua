4de8ca3d9baf5164e9ee6649b416ad7abab78308
Griffin Solimini <griffins@microsoft.com>
2024-04-01 08:47:55 -0700

    fix bootstrap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,8 +3,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
-    'ellisonleao/gruvbox.nvim',
-    'nvim-telescope/telescope.nvim', version = "0.1.6", dependencies = { 'nvim-lua/plenary.nvim' },
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
